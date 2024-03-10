6149921937fd88b8c2345fb5d9372f45eadcf00d
Bàn Văn Hiếu <92789973+bvhieu@users.noreply.github.com>
2024-03-10 08:44:34 +0700

    Server (#2)
    * connect db
    
    * [dev]:connect to db
    
    * [dev]: Remove node_modules from tracking
    
    * [dev]: test protect

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./db/connect");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello test server!");
+  res.send("Hello test protect server!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const startServer = async () => {
+  try {
+    const port = process.env.PORT || 5000;
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.log("Error while starting the server 💥", error);
+  }
+};
+
+startServer();
