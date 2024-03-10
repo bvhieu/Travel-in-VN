3a35a8128b0bbae06d085ad3cde1231dd57962f3
bvhieu <1732002hieu@gmail.com>
2024-03-10 07:59:09 +0700

    connect db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./db/connect");
 const app = express();
 const port = 3000;
 
@@ -9,3 +11,15 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const startServer = async () => {
+  try {
+    const port = process.env.PORT || 5000;
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.log("Error while starting the server 💥", error);
+  }
+};
+
+startServer();
